8b03e61fa68af8cd3d998b20dacd0f572977a15f
parshuram-patil <parshuram.patil@outlook.in>
2025-01-31 20:53:12 +0530

    add logs for APIs in service

--- api/src/main/java/com/reliaquest/api/service/EmployeeService.java
+++ api/src/main/java/com/reliaquest/api/service/EmployeeService.java
@@ -21,39 +21,48 @@ public class EmployeeService {
     private final EmployeeDataService dataService;
 
     public List<EmployeeEntity> getAllEmployees() {
+        log.info("Fetching all employees");
         return dataService.getAllEmployees();
     }
 
     public EmployeeEntity getEmployee(UUID id) {
+        log.info("Fetching employee with id {}", id);
         return dataService.getEmployee(id);
     }
 
     public EmployeeEntity createEmployee(CreateEmployeeRequestDto employeeRequest) {
+        log.info("Creating Employee with name {}", employeeRequest.name());
         return dataService.createEmployee(employeeRequest);
     }
 
     public Boolean deleteEmployee(String name) {
+        log.info("Deleting employee with name {}", name);
         return dataService.deleteEmployee(new DeleteEmployeeRequestDto(name));
     }
 
     public List<EmployeeEntity> getEmployeesByNameSearch(String searchString) {
+        log.info("Searching all employees with with pattern '{}'", searchString);
         return getAllEmployees().stream()
                 .filter(emp -> emp.employeeName().equalsIgnoreCase(searchString))
                 .toList();
     }
 
     public Integer getHighestSalaryOfEmployees() {
+        log.info("Finding highest salary of employees");
         Optional<EmployeeEntity> emp = getAllEmployees().stream()
                 .max(Comparator.comparingDouble(EmployeeEntity::employeeSalary));
 
         if(emp.isEmpty()) {
-            throw new EmployeeChallengeException("No Employees Found");
+            String errMsg = "No Employees Found";
+            log.error(errMsg);
+            throw new EmployeeChallengeException(errMsg);
         }
 
         return emp.get().employeeSalary();
     }
 
     public List<String> getTopTenHighestEarningEmployeeNames() {
+        log.info("Fetching top 10 highest earning employee names");
         return getAllEmployees().stream()
                 .sorted((e1, e2) -> Double.compare(e2.employeeSalary(), e1.employeeSalary()))
                 .limit(10)
